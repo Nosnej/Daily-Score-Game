1860b2a0dfe5912fb6704d8266b15c8aed4fdd0e
Nosnej <jensondavy@outlook.com>
2025-08-09 15:21:52 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Simple service worker for offline caching
+const CACHE_NAME = "dsg-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.svg"
+];
+self.addEventListener("install", (e)=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener("activate", (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME && caches.delete(k))))
+  );
+});
+self.addEventListener("fetch", (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(res=> res || fetch(e.request).then(resp=>{
+      // cache new GET requests
+      if(e.request.method==="GET"){
+        const clone = resp.clone();
+        caches.open(CACHE_NAME).then(c=>c.put(e.request, clone));
+      }
+      return resp;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
